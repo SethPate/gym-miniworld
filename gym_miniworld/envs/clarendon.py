f8eb373f310981e58d09eff165b683cf9280ac81
Seth Pate <swpate@icloud.com>
2020-12-13 19:19:27 -0500

    clarendon map complete

--- gym_miniworld/envs/clarendon.py
+++ gym_miniworld/envs/clarendon.py
@@ -35,30 +35,30 @@ class Clarendon(MiniWorldEnv):
         ft = 'floor_tiles_bw'
         # room x,x,z,z
         coords = [
-            (0,5,0,4, 'wood','concrete','concrete_tiles','stucco'),
-            (0,2,4,6, 'grass','cinder_blocks','ceiling_tiles',False),
-            (2,3,4,7, 'asphalt','marble',cont,False),
-            (3,5,4,7, 'wood_planks','brick_wall',cont,False),
-            (3,5,7,9,'wood_planks','drywall',cont,False),
-            (0,3,7,9,'asphalt',con,cont,False),
-            (0,8,9,14,'wood',con,cont,False),
-            (2,8,14,19,'wood',con,cont,False),
-            (4,8,19,21,'wood',con,cont,False),
-            (8,13,8,18,'marble','brick_wall',cont,True),
-            (8,21,18,21,'grass','brick_wall',cont,True),
+            (0,5,0,4, 'grass','cinder_blocks','concrete_tiles',True),
+            (0,3,4,6, 'grass','cinder_blocks','ceiling_tiles',True),
+            (2,3,6,7, 'grass','cinder_blocks',cont,True),
+            (3,5,4,7, 'wood_planks','brick_wall','wood_planks',False),
+            (3,5,7,9,'wood_planks','brick_wall','wood_planks',False),
+            (0,3,7,9,'wood','drywall','wood_planks',False),
+            (0,8,9,14,'wood','brick_wall','wood_planks',False),
+            (2,8,14,19,'wood','brick_wall','wood_planks',False),
+            (4,8,19,21,'wood','brick_wall','wood_planks',False),
+            (8,13,8,18,'grass','cinder_blocks',cont,True),
+            (8,21,18,21,'grass','cinder_blocks',cont,True),
             (13,16,13,18,'water','marble',cont,True),
-            (13,16,11,13,'grass','brick_wall',cont,True),
-            (16,21,8,18,'marble','brick_wall',cont,True),
-            (21,25,14,21,ft,con,cont,False),
-            (25,26,15,17,'wood',con,cont,False),
-            (21,25,12,14,'wood_planks',con,cont,False),
-            (21,25,9,12,ft,con,cont,False),
-            (25,29,9,15,'asphalt',con,cont,False),
-            (25,27,7,9,'asphalt',con,cont,False),
-            (27,29,7,9,'wood_planks',con,cont,False),
-            (25,27,4,7,'marble',con,cont,False),
-            (27,29,4,7,'wood_planks',con,cont,False),
-            (25,29,0,4,'wood',con,cont,False)
+            (13,16,11,13,'grass','cinder_blocks',cont,True),
+            (16,21,8,18,'grass','cinder_blocks',cont,True),
+            (21,25,14,21,ft,'brick_wall','drywall',False),
+            (25,27,15,17,'wood','brick_wall','drywall',False),
+            (21,25,12,14,'wood_planks','brick_wall',cont,False),
+            (21,25,9,12,ft,'brick_wall',cont,False),
+            (25,29,9,15,'wood_planks','drywall','stucco',False),
+            (25,27,7,9,'wood','drywall','stucco',False),
+            (27,29,7,9,'wood_planks','drywall','stucco',False),
+            (25,27,4,7,'wood','drywall','stucco',False),
+            (27,29,4,7,'wood_planks','drywall','stucco',False),
+            (25,29,0,4,'wood_planks','brick_wall','stucco',False)
         ]
 
         coords = [(x[0],x[1],-x[3],-x[2],x[4],x[5],x[6],x[7]) for x in coords]
@@ -80,7 +80,7 @@ class Clarendon(MiniWorldEnv):
         raw_connections = [
             (False, 1, 2, 0, 2),
             (False,1,4,3,5),
-            (True,3,4,5,7),
+            #(True,3,4,5,7),
             (False,4,5,3,5),
             (False,5,7,3,5),
             (False,6,7,0,2),
@@ -122,12 +122,6 @@ class Clarendon(MiniWorldEnv):
             else:
                 self.connect_rooms(rooms[r1], rooms[r2], min_x=x1, max_x=x2)
 
-        #self.box = self.place_entity(Box(color='red'))
-        #self.yellow_box = self.place_entity(Box(color='yellow', size=[0.8, 1.2, 0.5]))
-        #self.place_entity(Box(color='green', size=0.6))
-
-        # Mila logo image on the wall
-        
         hor = math.pi/2
         ver = math.pi
 
@@ -137,6 +131,19 @@ class Clarendon(MiniWorldEnv):
 
         paintings = [
             ([3,mid,-8], ver, 1, 'byron'),
+            ([3,mid,-5], ver, 1, 'adelaide_hanscom'),
+            #([3,mid,-5], ver, 1, 'alessandro_allori'),
+            ([4,mid,-14], hor, 1, 'alexandre_cabanel'),
+            ([2,mid,-15], ver, 1, 'alexei_harlamov'),
+            ([10,mid,-18], hor, 1, 'alexey_petrovich_antropov'),
+            ([10,mid,-8], hor, 1, 'alice_pike_barney'),
+            ([18,mid,-18], hor, 1, 'aman_theodor'),
+            ([18,mid,-8], hor, 1, 'antonello_messina'),
+            ([26,mid,-17], hor*3, 1, 'antonio_herrera_toro'),
+            ([24,mid,-12], hor*3, 1, 'cramacj_lucas'),
+            ([26,mid,-7], hor*3, 1, 'alexei_harlamov'),
+            ([26,mid,-7], hor, 1, 'benjamin_constant'),
+            ([26,mid,-4], hor*3, 1, 'carl_frederic_breda'),
             ]
 
         for p in paintings:
@@ -174,12 +181,12 @@ class Clarendon(MiniWorldEnv):
 
         entities = [
             (0, [2,0,-1], hor),
-            (7, [4,0,-11], ver),
+            (7, [4,0,-12], ver),
             (9, [5,0,-19], ver),
-            (10, [11,0,-15], ver),
+            (10, [12,0,-16], ver),
             (14, [17,0,-15], hor),
             (15, [24,0,-20], ver),
-            (19, [28,0,-12], ver),
+            (19, [28,0,-13], ver),
             (24, [26,0,-1], ver),
         ]
 
@@ -192,7 +199,7 @@ class Clarendon(MiniWorldEnv):
                 dir=d
                 )
 
-        self.place_agent(min_x=0,max_x=1,min_z=0,max_z=-1)
+        self.place_agent(dir=hor,min_x=28,max_x=29,min_z=0,max_z=-1)
 
     def step(self, action):
         obs, reward, done, info = super().step(action)
